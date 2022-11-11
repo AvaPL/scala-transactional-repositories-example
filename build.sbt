f1b9b458c5e42ff7da21db656a94ac280446f2b5
Paweł Cembaluk <241574@student.pwr.edu.pl>
2022-11-11 13:12:28 +0100

    Add unit test example.

--- build.sbt
+++ build.sbt
@@ -6,6 +6,8 @@ lazy val root = (project in file("."))
   .settings(
     name := "scala-transactional-repositories-example",
     libraryDependencies ++= Seq(
+      "org.scalactic" %% "scalactic" % "3.2.14",
+      "org.scalatest" %% "scalatest" % "3.2.14" % Test,
       "org.typelevel" %% "cats-effect" % "3.3.14",
       "org.tpolecat" %% "doobie-core" % "1.0.0-RC1",
       "org.tpolecat" %% "doobie-postgres" % "1.0.0-RC1"
---
+++ src/test/scala/io/github/avapl/service/AccountManagementServiceTest.scala
@@ -0,0 +1,45 @@
+package io.github.avapl.service
+
+import cats.{Id, ~>}
+import cats.arrow.FunctionK
+import io.github.avapl.repository.{AccountRepository, PointsRepository}
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.wordspec.AnyWordSpec
+
+import java.util.UUID
+
+class AccountManagementServiceTest extends AnyWordSpec with Matchers {
+
+  "addFunds" when {
+    "given an user ID and an amount to add" should {
+      "call the account repository with the amount and increment points in the points repository" in {
+        val testUserId = UUID.randomUUID()
+        val testAmountToAdd = 123
+        val accountRepository = new AccountRepository[Id] {
+          def addFunds(userId: UUID, amountToAdd: Int): Unit = {
+            userId shouldBe testUserId
+            amountToAdd shouldBe testAmountToAdd
+            ()
+          }
+
+          def getBalance(userId: UUID): Option[Int] = ???
+        }
+        val pointsRepository = new PointsRepository[Id] {
+          def incrementPoints(userId: UUID): Unit = {
+            userId shouldBe testUserId
+            ()
+          }
+
+          def getPoints(userId: UUID): Option[Int] = ???
+        }
+
+        given (Id ~> Id) = FunctionK.id[Id]
+
+        val accountManagementService =
+          AccountManagementService(accountRepository, pointsRepository)
+
+        accountManagementService.addFunds(testUserId, testAmountToAdd)
+      }
+    }
+  }
+}
